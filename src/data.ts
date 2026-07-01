30a5fdd3128fddf3b8420de26d7c6d8c1226cd65
Cartoon Kritthapath Yaviraj (Toonshouin!) <krittapath6699@gmail.com>
2026-07-01 13:36:51 +0700

    update: clean some trash (1/2)

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,4 @@
-export const fullVersionLink = 'https://resume.toonstorytime.me';
-export const sourceLink = 'https://github.com/toonnongaoey';
+export const fullVersionLink = 'https://resume.toonshou.in';
+export const sourceLink = 'https://github.com/toonshouin';
 export const ogImageUrl =
 	'https://file.smilekrub.net/file/sk-blob/web/user/avatar/cpr0ah9567t7x7ntknpzjx2631b4s6h4';
